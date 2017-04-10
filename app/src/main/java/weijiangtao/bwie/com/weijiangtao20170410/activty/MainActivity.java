2ac1699f7981d195056f51afbf9c3424a13d5ed1
weijiangtao139300 <18531027112@163.com>
2017-04-10 10:26:00 +0800

    Initial commit

---
+++ app/src/main/java/weijiangtao/bwie/com/weijiangtao20170410/activty/MainActivity.java
@@ -0,0 +1,23 @@
+package weijiangtao.bwie.com.weijiangtao20170410.activty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+import weijiangtao.bwie.com.weijiangtao20170410.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sayview);
+
+
+        
+        
+        
+        
+    }
+}
